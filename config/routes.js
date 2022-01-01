e8d9a13c58a3fdee896aecac7e8d9658e85930dc
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-01-01 18:41:58 -0300

    knowledge-cod3r

--- config/routes.js
+++ config/routes.js
@@ -10,6 +10,10 @@ module.exports = app => {
        .get(app.api.category.get)
        .post(app.api.category.save)
 
+   //Cuidado com ordem! tem que vir antes de /categories/:id  
+   app.route('/categories/tree')
+      .get(app.api.category.getTree)
+
    app.route('/categories/:id')
        .get(app.api.category.getById)
        .put(app.api.category.save)
